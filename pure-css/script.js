06b1ebab57483e3fd8aef95139da779c0b351d49
Zakharov Stepan <stepanzh@gmail.com>
2019-03-24 16:42:27 +0300

    new template

---
+++ pure-css/script.js
@@ -0,0 +1,17 @@
+//function inputsRange_init(){
+//    var inputsRange = document.getElementsByClassName("input--range");
+
+//    var i, inp;
+
+//    for (i = 0; i < inputsRange.length; i += 1) {
+//        inp = inputsRange[i];
+//        console.log(inp.dataset);
+//        
+//        // add event listeners
+//        inp.addEventListener();
+//    }
+//}
+
+//function input_onchange(e){
+//    console.log(e);
+//}

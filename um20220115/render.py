b36880184aefa84d23efbdf6799539cc4efb5b3c
Степан Захаров <stepanzh@gmail.com>
2022-01-16 17:09:19 +0300

    [um20220115] init

---
+++ um20220115/render.py
@@ -0,0 +1,19 @@
+from collections import namedtuple
+# import sys
+from jinja2 import Template
+
+template_path = "index.html.jinja"
+songstsv = "songlist.txt"
+
+Song = namedtuple("Song", ["singer", "title"])
+
+with open(template_path) as template_src:
+    temp = Template(template_src.read())
+
+with open(songstsv) as file:
+    songlist = []
+    for line in file:
+        singer, title = line.strip().split(" -- ")
+        songlist.append(Song(singer, title))
+    rendered = temp.render(songlist=songlist)
+    print(rendered)
